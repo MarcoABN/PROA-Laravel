b43d9211e475122c83a3a3e947142c91a7d9056f
MarcoABN <marcoanunes23@gmail.com>
2026-01-23 23:35:44 -0300

    commmit

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\AnexoController;
 use App\Http\Controllers\SiteController;
 use App\Livewire\Auth\LoginCpf;
 use App\Livewire\SimuladoNaval;
+use Spatie\Sitemap\SitemapGenerator;
 
 /*
 |--------------------------------------------------------------------------
@@ -44,4 +45,9 @@ Route::middleware(['auth:web'])->prefix('admin')->group(function () {
 Route::middleware(['auth:web'])->group(function () {
     Route::get('/propostas/{id}/imprimir', [PropostaController::class, 'imprimir'])
         ->name('propostas.imprimir');
+});
+
+Route::get('/gerar-sitemap', function () {
+    SitemapGenerator::create('https://campeaonautica.com.br')->writeToFile(public_path('sitemap.xml'));
+    return 'Sitemap gerado com sucesso!';
 });
